0e2ef0d1a842b1dbca1f6d356b80932a93e3c07e
Samir Silwal <samirsilwal2424@gmail.com>
2020-03-24 10:52:44 +0545

    Config webpack for the application

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/app/app.tsx",
+
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+
+    resolve: {
+        exetensions:[".ts", ".tsx", ".js", ".json", ".jsx"]
+    },
+
+    module: {
+        rules:[
+            {test: /\.(ts|tsx)$/, loader: "ts-loader"}
+        ]
+    },
+
+    plugins:[
+        new HTMLWebpackPlugin(
+            {
+                template: path.resolve(__dirname, "src", "app", "index.html")
+            }
+        )
+    ]
+}
